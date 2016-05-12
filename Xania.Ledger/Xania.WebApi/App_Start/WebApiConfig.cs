1567edc74407dca3b7a46b673540dfc847778f05
Ibrahim ben Salah <ibrahim.bensalah@gmail.com>
2016-05-12 23:42:03 +0200

    upload control spike

--- Xania.Ledger/Xania.WebApi/App_Start/WebApiConfig.cs
+++ Xania.Ledger/Xania.WebApi/App_Start/WebApiConfig.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
+﻿using System.Configuration;
 using System.Web.Hosting;
 using System.Web.Http;
-using System.Web.Http.Dependencies;
 using Xania.DataAccess;
 using Xania.IoC;
 using Xania.IoC.Resolvers;
