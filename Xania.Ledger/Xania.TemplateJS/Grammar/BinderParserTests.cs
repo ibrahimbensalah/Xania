d5fde978726b335424ca7c9a2e4c54ea4c9bfbd3
Ibrahim ben Salah <ibrahim.bensalah@gmail.com>
2016-05-18 19:24:41 +0200

    add BinderParserTests

---
+++ Xania.Ledger/Xania.TemplateJS/Grammar/BinderParserTests.cs
@@ -0,0 +1,24 @@
+﻿using Antlr4.Runtime;
+using System;
+using System.IO;
+using Antlr4.Runtime.Tree;
+using NUnit.Framework;
+
+namespace Xania.Binder.Grammar
+{
+    [TestFixture]
+    public class BinderParserTests
+    {
+        [Test]
+        public static void Test()
+        {
+            ICharStream input = new AntlrInputStream(new StringReader("@asdfas33.ddd @asdfasd.f66.hhh"));
+            var lex = new BinderLexer(input);
+            var tokens = new CommonTokenStream(lex);
+            var parser = new BinderParser(tokens);
+
+            Console.WriteLine(parser.binderExpr().ToStringTree(parser));
+            Console.WriteLine(parser.binderExpr().ToStringTree(parser));
+        }
+    }
+}
