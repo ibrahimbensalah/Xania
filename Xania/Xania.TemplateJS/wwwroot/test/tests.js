6e92487aa96ff962285aa12f3ad3deb13577fca5
Ibrahim ben Salah <ibrahim.bensalah@gmail.com>
2016-12-18 19:32:21 +0100

    sample tests written in typescript

---
+++ Xania/Xania.TemplateJS/wwwroot/test/tests.js
@@ -0,0 +1,8 @@
+/// <reference path="../../node_modules/@types/jasmine/index.d.ts" />
+describe("asd fasdf asd", function () {
+    it("expectation", function () {
+        console.log("testa sdf as");
+    });
+});
+
+window.ReSharperReporter.prototype.jasmineDone = function () { };
---
+++ Xania/Xania.TemplateJS/wwwroot/test/tests.ts
@@ -0,0 +1,9 @@
+﻿/// <reference path="../../node_modules/@types/jasmine/index.d.ts" />
+describe("asd fasdf asd", () => {
+    it("expectation",
+        () => {
+            console.log("testa sdf as");
+        });
+});
+var ReSharperReporter;
+ReSharperReporter.prototype.jasmineDone = function () { };
